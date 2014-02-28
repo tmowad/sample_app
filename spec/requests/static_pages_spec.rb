34ff52624c4da776c248b5b0562b703159f28c4a
Tom Mowad <tmowad@gmail.com>
2014-02-27 18:30:40 -0800

    Some changes from rails tutorial chapter 4.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:title) { $title = "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title(title)
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -21,7 +27,7 @@ describe "Static pages" do
     
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title(title + " | Help")
     end
   end
 
@@ -33,7 +39,19 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title(title + " | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{title} | Contact Us")
     end
   end
 end
